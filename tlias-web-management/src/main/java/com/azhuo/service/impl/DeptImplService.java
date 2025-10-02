6b6cc448d58d0b8d01a1392583d71c8952c022bc
azhuo98 <945577536@qq.com>
2025-10-02 18:00:06 +0800

    Initial Commit

---
+++ tlias-web-management/src/main/java/com/azhuo/service/impl/DeptImplService.java
@@ -0,0 +1,8 @@
+package com.azhuo.service.impl;
+
+import com.azhuo.service.DeptService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeptImplService implements DeptService {
+}
