3a191dd54e9b3383c024a1cfa1f8c63c15240e97
azhuo98 <945577536@qq.com>
2025-10-07 17:32:11 +0800

    feat: @ConfigurationProperties(prefix = "aliyun.oss")注解优化阿里云OSS配置

--- tlias-web-management/src/main/java/com/azhuo/utils/AliyunOSSOperator.java
+++ tlias-web-management/src/main/java/com/azhuo/utils/AliyunOSSOperator.java
@@ -4,7 +4,7 @@ import com.aliyun.oss.*;
 import com.aliyun.oss.common.auth.CredentialsProviderFactory;
 import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
 import com.aliyun.oss.common.comm.SignVersion;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.io.ByteArrayInputStream;
 import java.time.LocalDate;
@@ -13,13 +13,22 @@ import java.util.UUID;
 
 @Component
 public class AliyunOSSOperator {
+
     // 从application.yml配置文件中读取阿里云OSS配置
-    @Value("${aliyun.oss.endpoint}")
+    // 方法1：使用@Value注解从application.yml配置文件中读取阿里云OSS配置
+    /*@Value("${aliyun.oss.endpoint}")
     private String endpoint;
     @Value("${aliyun.oss.bucketName}")
     private String bucketName;
     @Value("${aliyun.oss.region}")
-    private String region;
+    private String region;*/
+
+    // 方法2：使用@Autowired注解自动注入AliyunOSSProperties配置类
+    @Autowired
+    private final AliyunOSSProperties aliyunOSSProperties;
+     public AliyunOSSOperator(AliyunOSSProperties aliyunOSSProperties) {
+        this.aliyunOSSProperties = aliyunOSSProperties;
+    }
 
     public String upload(byte[] content, String originalFilename) throws Exception {
         // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
@@ -32,6 +41,11 @@ public class AliyunOSSOperator {
         //生成一个新的不重复的文件名
         String newFileName = UUID.randomUUID() + originalFilename.substring(originalFilename.lastIndexOf("."));
         String objectName = dir + "/" + newFileName;
+
+        // 从AliyunOSSProperties配置类中获取endpoint、bucketName、region
+        String endpoint = aliyunOSSProperties.getEndpoint();
+        String bucketName = aliyunOSSProperties.getBucketName();
+        String region = aliyunOSSProperties.getRegion();
 
         // 创建OSSClient实例。
         ClientBuilderConfiguration clientBuilderConfiguration = new ClientBuilderConfiguration();
