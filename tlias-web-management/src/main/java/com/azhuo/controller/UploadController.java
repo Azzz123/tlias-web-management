c7f12bc37eb94aa651d01792684beaa329b90688
azhuo98 <945577536@qq.com>
2025-10-07 17:11:06 +0800

    feat: 阿里云OSS上传文件工具类的初步配置

--- tlias-web-management/src/main/java/com/azhuo/controller/UploadController.java
+++ tlias-web-management/src/main/java/com/azhuo/controller/UploadController.java
@@ -1,25 +1,26 @@
 package com.azhuo.controller;
 
 import com.azhuo.pojo.Result;
+import com.azhuo.utils.AliyunOSSOperator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.util.UUID;
 
 @Slf4j
 @RestController
 @RequestMapping("/upload")
 public class UploadController {
 
-    @PostMapping
+    // ----------------------将上传的文件保存到本地磁盘指定位置----------------------
+    /*@PostMapping
     public Result upload(String name, Integer age, MultipartFile file) {
         log.info("上传文件, 接收参数name={}, age={}, file={}", name, age, file);
 
-        // ----------------------将上传的文件保存到服务器指定位置----------------------
+
         // 1. 获取原始文件名
         String originalFilename = file.getOriginalFilename();
 
@@ -41,5 +42,22 @@ public class UploadController {
             return Result.error("上传文件失败");
         }
         return Result.success();
+    }*/
+
+    // ----------------------将上传的文件保存到阿里云OSS----------------------
+    @Autowired
+    private final AliyunOSSOperator aliyunOSSOperator;
+    public UploadController(AliyunOSSOperator aliyunOSSOperator) {
+        this.aliyunOSSOperator = aliyunOSSOperator;
+    }
+
+    @PostMapping
+    public Result upload(MultipartFile file) throws Exception {
+        log.info("上传文件, 接收参数file={}", file.getOriginalFilename());
+
+        // 1. 调用aliyunOSSOperator.upload()方法，将文件上传到阿里云OSS
+        String url = aliyunOSSOperator.upload(file.getBytes(), file.getOriginalFilename());
+        // 2. 返回上传成功的结果
+        return Result.success(url);
     }
 }
