52296f8c4438c4ca68e9fb48b3a45ffb0e4fe959
azhuo98 <945577536@qq.com>
2025-10-08 20:20:12 +0800

    refactor(persistence): 将DeptMapper和EmpLogMapper迁移至XML配置 此变更不涉及任何业务逻辑的修改，仅为内部实现方式的优化。

--- tlias-web-management/src/main/java/com/azhuo/mapper/DeptMapper.java
+++ tlias-web-management/src/main/java/com/azhuo/mapper/DeptMapper.java
@@ -8,11 +8,11 @@ import java.util.List;
 @Mapper
 public interface DeptMapper {
     // 查询全部部门数据
-    @Select("SELECT id, name, create_time, update_time FROM dept ORDER BY update_time DESC")
+//    @Select("SELECT id, name, create_time, update_time FROM dept ORDER BY update_time DESC")
     List<Dept> findAll();
 
     // 删除部门
-    @Delete("DELETE FROM dept WHERE id = #{id}")
+//    @Delete("DELETE FROM dept WHERE id = #{id}")
     void delete(Integer id);
 
     // 新增部门
@@ -23,13 +23,13 @@ public interface DeptMapper {
     // 这样就可以在SQL语句中使用#{paramName}来引用参数值
     // 例如：@Param("name") String name
     // 然后在SQL语句中使用#{name}来引用参数值
-    @Insert("INSERT INTO dept(name, create_time, update_time) VALUES (#{name}, #{createTime}, #{updateTime})")
+//    @Insert("INSERT INTO dept(name, create_time, update_time) VALUES (#{name}, #{createTime}, #{updateTime})")
     void insert(Dept dept);
 
-    @Select("SELECT id, name, create_time, update_time FROM dept WHERE id = #{id}")
+//    @Select("SELECT id, name, create_time, update_time FROM dept WHERE id = #{id}")
     Dept getById(Integer id);
 
      // 更新部门
-    @Update("UPDATE dept SET id=#{id}, name=#{name}, update_time=#{updateTime} WHERE id = #{id}")
+//    @Update("UPDATE dept SET id=#{id}, name=#{name}, update_time=#{updateTime} WHERE id = #{id}")
     void update(Dept dept);
 }
