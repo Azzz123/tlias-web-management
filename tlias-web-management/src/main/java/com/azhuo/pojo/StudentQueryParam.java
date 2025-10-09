5de6b53a7ff5b6aa0b0b9fa72147161780e2862f
azhuo98 <945577536@qq.com>
2025-10-10 00:40:22 +0800

    chore: add no/all Constructor

--- tlias-web-management/src/main/java/com/azhuo/pojo/StudentQueryParam.java
+++ tlias-web-management/src/main/java/com/azhuo/pojo/StudentQueryParam.java
@@ -1,8 +1,12 @@
 package com.azhuo.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentQueryParam {
     private Integer page = 1;
     private Integer pageSize = 10;
